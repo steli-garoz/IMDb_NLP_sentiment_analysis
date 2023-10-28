34a18eea90b074bc7795959da5ae260569f7045b
steli-garoz <steligaroz@gmail.com>
2023-10-28 08:55:33 +0200

    Applied Logistic Regression as a first algorithm to solve the problem

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ Created on Sat Oct 28 03:46:09 2023
 import spacy
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
 
 
 # Path to IMDb reviews dataset file
@@ -15,8 +18,12 @@ dataset_path = 'imdb_dataset.csv'
 
 data = pd.read_csv(dataset_path)
 
+# Replace the string value with 0, 1
+data['sentiment'] = data['sentiment'].map({'negative': 0, 'positive': 1})
+
 # Use less data for development reasons
-data = data.iloc[0:10, :]
+data = data.iloc[0:1500, :]
+labels = data['sentiment'][:1500]
 
 # Load the language model
 nlp = spacy.load("en_core_web_sm")
@@ -52,3 +59,38 @@ tfidf_matrix = tfidf_vectorizer.fit_transform(data['lemmatized_text'].apply(lamb
 # You now have a TF-IDF matrix containing numerical representations of your text data
 print(tfidf_matrix.shape)
 
+# Assuming 'tfidf_matrix' is your TF-IDF feature matrix, and 'labels' is your sentiment labels (0 for negative, 1 for positive)
+X_train, X_test, y_train, y_test = train_test_split(tfidf_matrix, labels, test_size=0.2, random_state=42)
+
+# Create a logistic regression model
+logistic_regression_model = LogisticRegression()
+
+# Train the model on the training data
+logistic_regression_model.fit(X_train, y_train)
+
+# Make predictions on the test data
+y_pred = logistic_regression_model.predict(X_test)
+
+# Calculate evaluation metrics
+accuracy = accuracy_score(y_test, y_pred)
+precision = precision_score(y_test, y_pred)
+recall = recall_score(y_test, y_pred)
+f1 = f1_score(y_test, y_pred)
+conf_matrix = confusion_matrix(y_test, y_pred)
+
+print("Accuracy:", accuracy)
+print("Precision:", precision)
+print("Recall:", recall)
+print("F1 Score:", f1)
+print("Confusion Matrix:")
+print(conf_matrix)
+
+new_text = ["This is a shit movie... Don't worth it. I would prefer to eat bananas with onion. Disaster"]
+
+# Transform the new text using the TF-IDF vectorizer
+new_text_tfidf = tfidf_vectorizer.transform(new_text)
+
+# Predict sentiment
+predicted_sentiment = logistic_regression_model.predict(new_text_tfidf)
+
+print("Predicted Sentiment:", "Positive" if predicted_sentiment[0] == 1 else "Negative")
