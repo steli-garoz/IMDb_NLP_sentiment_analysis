68b7d1067eeb2442dc89c6dbf3769c98585f4857
steli-garoz <steligaroz@gmail.com>
2023-10-28 08:09:44 +0200

    Removed stopwords

--- main.py
+++ main.py
@@ -13,20 +13,23 @@ dataset_path = 'imdb_dataset.csv'
 
 data = pd.read_csv(dataset_path)
 
+# Use less data for development reasons
+data = data.iloc[0:10, :]
+
 # Load the language model
 nlp = spacy.load("en_core_web_sm")
 
 # Tokenize the text and store it in a new column 'tokenized_text'
 data['tokenized_text'] = None  # Create an empty column to store tokenized text
 
-# Add a counter to track progress
-total_reviews = len(data)
+# Remove stopwords and create a list of non-stopwords for each review
 for i, text in enumerate(data['review']):
-    tokenized_text = [token.text for token in nlp(text)]
-    data.at[i, 'tokenized_text'] = tokenized_text
+    doc = nlp(text)
+    non_stopwords = [token.text for token in doc if not token.is_stop]
+    data.at[i, 'tokenized_text'] = non_stopwords
 
     # Print a visual counter
-    print(f"Tokenizing review {i + 1} of {total_reviews}")
+    print(f"Tokenizing review {i + 1} of {len(data)}")
 
 # Now, the 'tokenized_text' column contains lists of tokens for each review.
 print(data['tokenized_text'].head())
