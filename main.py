6ee12a1069986dd777ded3d0d4035348ff98c827
steli-garoz <steligaroz@gmail.com>
2023-10-29 03:05:37 +0100

    Added extra validation step for the applied Logistic Regression

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
 
-
 # Path to IMDb reviews dataset file
 dataset_path = 'imdb_dataset.csv'
 
@@ -22,42 +21,49 @@ data = pd.read_csv(dataset_path)
 data['sentiment'] = data['sentiment'].map({'negative': 0, 'positive': 1})
 
 # Use less data for development reasons
-data = data.iloc[0:1500, :]
-labels = data['sentiment'][:1500]
+train_data = data.iloc[0:1000, :].copy()
+labels = train_data['sentiment']
+val_data = data.iloc[1000:2000, :].copy()
+val_labels = val_data['sentiment']
+
+num_documents = len(val_data)
+print("Number of documents in val_data:", num_documents)
 
 # Load the language model
 nlp = spacy.load("en_core_web_sm")
 
-# Tokenize the text and store it in a new column 'tokenized_text'
-data['tokenized_text'] = None  # Create an empty column to store tokenized text
+# Create lists to store tokenized and lemmatized text
+train_data['tokenized_text'] = [None] * len(train_data)
+train_data['lemmatized_text'] = [None] * len(train_data)
+val_data['tokenized_text'] = [None] * len(val_data)
+val_data['lemmatized_text'] = [None] * len(val_data)
 
-# Remove stopwords and create a list of non-stopwords for each review
-for i, text in enumerate(data['review']):
+# Tokenize, remove stopwords, lemmatize and create a list of non-stopwords for each review in train data
+for i, text in enumerate(train_data['review']):
     doc = nlp(text)
     non_stopwords = [token.text for token in doc if not token.is_stop]
-    data.at[i, 'tokenized_text'] = non_stopwords
+    train_data.at[i, 'tokenized_text'] = non_stopwords
+    train_data.at[i, 'lemmatized_text'] = [token.lemma_ for token in doc]
 
     # Print a visual counter
-    print(f"Tokenizing review {i + 1} of {len(data)}")
+    print(f"Tokenizing review {i + 1} of {len(train_data)}")
 
-# Now, the 'tokenized_text' column contains lists of tokens for each review.
-print(data['tokenized_text'].head())
+# Tokenize, remove stopwords, lemmatize and create a list of non-stopwords for each review in test data
+for i, text in enumerate(val_data['review']):
+    doc = nlp(text)
+    non_stopwords = [token.text for token in doc if not token.is_stop]
+    val_data.at[1000+i, 'tokenized_text'] = non_stopwords
+    val_data.at[1000+i, 'lemmatized_text'] = [token.lemma_ for token in doc]
 
-
-# Apply lemmatization to the tokenized text
-data['lemmatized_text'] = data['tokenized_text'].apply(lambda tokens: [token.lemma_ for token in nlp(" ".join(tokens))])
-
-# Now, the 'lemmatized_text' column contains lists of lemmatized tokens for each review.
-print(data['lemmatized_text'].head())
+    # Print a visual counter
+    print(f"Tokenizing test review {i + 1} of {len(val_data)}")
 
 # Create a TF-IDF vectorizer
 tfidf_vectorizer = TfidfVectorizer(max_features=5000)  # You can adjust the max_features as needed
 
 # Fit and transform the text data
-tfidf_matrix = tfidf_vectorizer.fit_transform(data['lemmatized_text'].apply(lambda x: ' '.join(x)))
-
-# You now have a TF-IDF matrix containing numerical representations of your text data
-print(tfidf_matrix.shape)
+tfidf_matrix = tfidf_vectorizer.fit_transform(train_data['lemmatized_text'].apply(lambda x: ' '.join(x)))
+tfidf_matrix_test = tfidf_vectorizer.transform(val_data['lemmatized_text'].apply(lambda x: ' '.join(x)))
 
 # Assuming 'tfidf_matrix' is your TF-IDF feature matrix, and 'labels' is your sentiment labels (0 for negative, 1 for positive)
 X_train, X_test, y_train, y_test = train_test_split(tfidf_matrix, labels, test_size=0.2, random_state=42)
@@ -85,12 +91,19 @@ print("F1 Score:", f1)
 print("Confusion Matrix:")
 print(conf_matrix)
 
-new_text = ["This is a shit movie... Don't worth it. I would prefer to eat bananas with onion. Disaster"]
+# Make predictions on the validation data
+val_predictions = logistic_regression_model.predict(tfidf_matrix_test)
 
-# Transform the new text using the TF-IDF vectorizer
-new_text_tfidf = tfidf_vectorizer.transform(new_text)
+# Calculate evaluation metrics for the validation data
+val_accuracy = accuracy_score(val_labels, val_predictions)
+val_precision = precision_score(val_labels, val_predictions)
+val_recall = recall_score(val_labels, val_predictions)
+val_f1 = f1_score(val_labels, val_predictions)
+val_conf_matrix = confusion_matrix(val_labels, val_predictions)
 
-# Predict sentiment
-predicted_sentiment = logistic_regression_model.predict(new_text_tfidf)
-
-print("Predicted Sentiment:", "Positive" if predicted_sentiment[0] == 1 else "Negative")
+print("Validation Accuracy:", val_accuracy)
+print("Validation Precision:", val_precision)
+print("Validation Recall:", val_recall)
+print("Validation F1 Score:", val_f1)
+print("Validation Confusion Matrix:")
+print(val_conf_matrix)
